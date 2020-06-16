b3bc0b9800324a290a9aa61d1db322b017aaac47
Priyanka Dadlani <dadlani.priyanka@gmail.com>
2020-06-16 13:26:32 -0700

    GET endpoint setup.

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const server = express();
 
 const PORT = 5000;
 
+// setting up the different routes
+server.get(`/`, (req, res) => {
+  res.json({ hello: "talls" })
+})
+
+server.get("/hello", (req, res) => {
+  res.json({ hello: "my love" })
+})
+
 server.listen(PORT, () => {
   console.log(`\n*** Server Running on http://localhost:${PORT}`);
 })
